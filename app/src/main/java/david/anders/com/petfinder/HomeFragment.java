229f5d3c514c8ca0673f185c3c134583859ea914
David Beer <davve@davids-air.clients.internal.mah.se>
2017-07-06 14:36:06 +0200

    first home page is almost done

--- app/src/main/java/david/anders/com/petfinder/HomeFragment.java
+++ app/src/main/java/david/anders/com/petfinder/HomeFragment.java
@@ -6,6 +6,9 @@ import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
 
 
 /**
@@ -13,6 +16,9 @@ import android.view.ViewGroup;
  */
 public class HomeFragment extends Fragment {
 
+    private Button btnLost;
+    private Button btnFound;
+    private Controller controller;
 
     public HomeFragment() {
         // Required empty public constructor
@@ -23,10 +29,40 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        initComponents(view);
+        registerListener();
+        return view;
+    }
+
+    private void initComponents(View view) {
+        btnLost = (Button)view.findViewById(R.id.btnLost);
+        btnFound = (Button)view.findViewById(R.id.btnFind);
+        LinearLayout layout =(LinearLayout)view.findViewById(R.id.bgFrame);
+        layout.setBackgroundResource(R.drawable.homebackground);
+    }
+
+    private void registerListener() {
+        btnFound.setOnClickListener(new ButtonListener());
+        btnLost.setOnClickListener(new ButtonListener());
     }
 
     public void setController(Controller controller) {
+        this.controller = controller;
+    }
 
+    private class ButtonListener implements View.OnClickListener {
+
+        @Override
+        public void onClick(View view) {
+            switch (view.getId()){
+                case R.id.btnLost :
+                    System.out.println("BTN LOSt");
+                    break;
+                case R.id.btnFind :
+                    System.out.println("BTN FOUND");
+                    break;
+            }
+        }
     }
 }
