4094ddbee77c12b287cd39da34a61c662b37bd57
anders.frid85@gmail.com <anders.frid85@gmail.com>
2017-07-12 13:23:41 +0200

    Nullpointer buggg

--- app/src/main/java/david/anders/com/petfinder/FindFragment.java
+++ app/src/main/java/david/anders/com/petfinder/FindFragment.java
@@ -1,6 +1,7 @@
 package david.anders.com.petfinder;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.app.Fragment;
 import android.view.LayoutInflater;
@@ -8,7 +9,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.LinearLayout;
+
 
 
 /**
@@ -32,6 +33,7 @@ public class FindFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_find, container, false);
+        registerListener();
         initComponents(view);
         return view;
     }
@@ -45,5 +47,28 @@ public class FindFragment extends Fragment {
     }
     public void setController(Controller controller){
         this.controller = controller;
+    }
+
+    private void registerListener() {
+        btnImage.setOnClickListener(new ButtonListener());
+        btnPost.setOnClickListener(new ButtonListener());
+    }
+
+    private class ButtonListener implements View.OnClickListener {
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()) {
+                case R.id.btnPost :
+                    String name = edName.getText().toString();
+                    String desc = edDesc.getText().toString();
+                    String phone = edPhone.getText().toString();
+                    break;
+                case R.id.btnImage :
+                    Intent i = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+                    final int ACTIVITY_SELECT_IMAGE = 1234;
+                    startActivityForResult(i, ACTIVITY_SELECT_IMAGE);
+                    break;
+            }
+        }
     }
 }
--- app/src/main/java/david/anders/com/petfinder/MainActivity.java
+++ app/src/main/java/david/anders/com/petfinder/MainActivity.java
@@ -4,7 +4,13 @@ import android.app.Activity;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 
 public class MainActivity extends Activity {
 
@@ -25,6 +31,29 @@ public class MainActivity extends Activity {
         fragmentTransaction.replace(R.id.container, fragment);
         fragmentTransaction.commit();
     }
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        super.onActivityResult(requestCode, resultCode, data);
 
+        switch(requestCode) {
+            case 1234:
+                if(resultCode == RESULT_OK){
+                    Uri selectedImage = data.getData();
+                    String[] filePathColumn = {MediaStore.Images.Media.DATA};
+
+                    Cursor cursor = getContentResolver().query(selectedImage, filePathColumn, null, null, null);
+                    cursor.moveToFirst();
+
+                    int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+                    String filePath = cursor.getString(columnIndex);
+                    cursor.close();
+
+
+                    Bitmap yourSelectedImage = BitmapFactory.decodeFile(filePath);
+            /* Now you have choosen image in Bitmap format in object "yourSelectedImage". You can use it in way you want! */
+                }
+        }
+
+    };
 
 }
