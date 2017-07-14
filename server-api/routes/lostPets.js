1b77e1d7ded05d9d45b91e4506d81548d0ac0c39
David Beer <davve@Davids-MacBook-Air.local>
2017-07-14 11:45:03 +0200

    working on backend and some fragment

--- server-api/routes/lostPets.js
+++ server-api/routes/lostPets.js
@@ -1,6 +1,18 @@
 const express = require('express');
 const router = express.Router();
+const multer  = require('multer');
+const path = require('path');
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './uploads');
+  },
+  filename: function (req, file, cb) {
+    console.log(file);
+    cb(null, file.fieldname + '-' + file.originalname);
+  }
+});
 
+const upload = multer({ storage: storage });
 
 //Loads al the lost pets
 router.get('/load-all', (req, res, next) => {
@@ -12,9 +24,14 @@ router.get('/load', (req, res, next) => {
   res.send('Loading a specific pet');
 });
 
-//Adds a new lost pet post
-router.post('/add', (req, res, next) => {
-  res.send('Post a new pet');
+//Adds a new lost pet post working?
+router.post('/upload', (req, res, next) => {
+  let upload = multer({
+		storage: storage
+	}).single('lostpet');
+	upload(req, res, function(err) {
+		res.send('File is uploaded');
+	});
 });
 
 //Adds image post of the found pet
